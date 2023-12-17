900e1774e955f3c1f05ae957c398598f2b30d6ca
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-12-17 10:47:41 +0100

    Configure react-router and add MainLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import MainLayout from "./components/MainLayout";
+
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<MainLayout />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
