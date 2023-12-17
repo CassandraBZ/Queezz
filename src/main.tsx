973634bee4ffad5f76f7b0c23123a4f0d757b1f9
KentriLuigi <kentriluigi@gmail.com>
2023-12-17 11:15:43 +0100

    set basename for the router

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Queezz">
       <App />
     </Router>
   </React.StrictMode>
