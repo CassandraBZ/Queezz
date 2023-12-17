a80b7f4f29871d2265578e0b459edbcd8dfec9af
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-12-17 17:52:04 +0100

    add random avatars, custom cards and modify icons stroke

--- src/components/AvatarCarousel.tsx
+++ src/components/AvatarCarousel.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 import json from "../json/avatars.json";
+import dice from "../assets/icons/lucide_dices.svg";
 
 function AvatarCarousel() {
   const { avatars } = json;
 
+  const [avatar, setAvatar] = useState(
+    avatars[Math.floor(Math.random() * avatars.length)]
+  );
+
+  const randomAvatar = () => {
+    const index = Math.floor(Math.random() * avatars.length);
+    setAvatar(avatars[index]);
+  };
+
   return (
-    <div className="carousel">
-      {avatars.map((avatar) => (
-        <button key={avatar.id} className="avatar">
-          <img src={avatar.url} />
-        </button>
-      ))}
+    <div className="avatar-container">
+      <div className="avatar">
+        <img src={avatar.url} />
+      </div>
+      <button type="button" className="random-btn" onClick={randomAvatar}>
+        <img src={dice} />
+      </button>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,17 @@ import AvatarCarousel from "../components/AvatarCarousel";
 
 function Home() {
   const description =
-    "Queezz est une application de quiz en ligne. Elle permet de créer des quiz et de les partager avec ses amis. Les quiz sont composés de 20 questions et durent 5 minutes. Les questions sont de type QCM (Question à Choix Multiples) ou à remplir. Êtes-vous prêt à devenir la reine des abeilles ? Alors, c'est parti !";
+    "Queezz vous permet de créer des quiz et de les partager avec vos amis. Les quiz sont composés de 20 questions et durent 5 minutes. Êtes-vous prêt à devenir la reine des abeilles ? Alors, c'est parti !";
   return (
-    <div>
-      <div>
-        <img src={logo} alt="Logo queezz" />
+    <div className="home-page">
+      <div className="home-header">
+        <img src={logo} alt="Logo queezz" className="logo" />
         <p>{description}</p>
       </div>
-      <div>
+      <div className="chooser">
         <input placeholder="Entrez votre pseudo" />
         <AvatarCarousel />
+        <button type="button">Créer une ruche</button>
       </div>
     </div>
   );
